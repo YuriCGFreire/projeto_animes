f4085001fb1897ad2a2333f58b121f9221b81ac5
Yuri Freire <yurifreireg@gmail.com>
2021-06-29 22:30:19 -0300

    criei a migration de posts

--- src/database/migrations/1624471606346-CreatePost.ts
+++ src/database/migrations/1624471606346-CreatePost.ts
@@ -1,11 +1,60 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreatePost1624471606346 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "posts",
+            columns:[
+                {
+                    name: "id",
+                    type: 'uuid',
+                    isPrimary: true,
+                    isNullable: false,
+                },
+                {
+                    name: "title",
+                    type: "varchar",
+                    isNullable: false
+                },
+                {
+                    name: "content",
+                    type: "text",
+                    isNullable: false
+                },
+                {
+                    name: "author_id",
+                    type: "uuid",
+                    isNullable: false,
+                },
+                {
+                    name: "like",
+                    type: "int"
+                },
+                {
+                    name: "dislike",
+                    type: "int"
+                },
+                {
+                    name: "middle_finger",
+                    type: "int"
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                },
+                {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+            ]
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("posts")
     }
 
 }
