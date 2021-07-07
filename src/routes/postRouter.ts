1e4b28c39b57305b39504e7c804663034b1a155b
Yuri Freire <yurifreireg@gmail.com>
2021-07-07 16:33:46 -0300

    Update postRouter.ts

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -6,8 +6,9 @@ const postController = new PostsController();
 postRouter.post('/', postController.create);
 postRouter.get('/:id', postController.findPostById);
 postRouter.get('/user/:id', postController.findPostsByUserId);
-/* postRouter.put('/like/:id', postController.oneMorelike)
-postRouter.put('/like/:id', postController.oneLessLike) */
-
+postRouter.put('/morelike/:id', postController.oneMorelike) //Para teste
+postRouter.put('/lesslike/:id', postController.oneLessLike) //Para teste
+postRouter.put('/moredislike/:id', postController.oneMoreDislike) //Para teste
+postRouter.put('/lessdislike/:id', postController.oneLessDislike) //Para teste
 
 export {postRouter};
