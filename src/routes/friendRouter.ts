c85c34b2e7dbe0163e94dc91dc1e9d1d89e99ab7
Yuri Freire <yurifreireg@gmail.com>
2021-07-09 01:54:41 -0300

    Update friendRouter.ts

--- src/routes/friendRouter.ts
+++ src/routes/friendRouter.ts
@@ -4,5 +4,7 @@ const friendRouter = Router()
 const friendsController = new FriendsController()
 
 friendRouter.post('/request', friendsController.request)
+friendRouter.get('/pendent', friendsController.getPendentRequests)
+friendRouter.put('/pendent', friendsController.acceptedOrRefused)
 
 export {friendRouter}
