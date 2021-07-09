30e61ea65dfd9f0374d3e171213967ba65520c11
Yuri Freire <yurifreireg@gmail.com>
2021-07-09 01:47:02 -0300

    Update FriendsService.ts

--- src/services/FriendsService.ts
+++ src/services/FriendsService.ts
@@ -21,8 +21,26 @@ export class FriendsService {
         return request
     }
 
-    async acceptedOrRefused(){
-        
+    async acceptedOrRefused(status: string, id:string){ //Id do convite
+        const request = await this.friendRepository.findOne({id})
+
+        if(status == "aceito"){
+            request.active = true
+        }
+
+        const updatedRequest = await this.friendRepository.save({
+            requester: request.user_id_requester,
+            requested: request.user_id_requested,
+            status: status,
+            connection: request.active
+        })
+
+        return updatedRequest
+    }
+
+    async getPendetRequests(user_id_requested:string){
+        const requests = await this.friendRepository.find({user_id_requested})
+        return requests
     }
 
 }
