5fcefafb642453871ca00f927591d0d6637c0464
Yuri Freire <yurifreireg@gmail.com>
2021-07-13 15:32:57 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { postRouter } from "./postRouter";
 import { userRouter } from "./userRouter";
-import { friendRouter } from "./friendRouter";
 const routes = Router()
 
 routes.use('/user', userRouter)
 routes.use('/post', postRouter)
-routes.use('/friend', friendRouter)
 
 export {routes}
