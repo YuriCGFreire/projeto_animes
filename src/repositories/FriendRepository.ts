bd39eb9f1084ded334e1b4b6bf6366e35bfa9aa6
Yuri Freire <yurifreireg@gmail.com>
2021-07-13 16:43:12 -0300

    Delete FriendRepository.ts

--- src/repositories/FriendRepository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Friend } from "../models/Friend";
-
-@EntityRepository(Friend)
-export class FriendRepository extends Repository<Friend>{}
